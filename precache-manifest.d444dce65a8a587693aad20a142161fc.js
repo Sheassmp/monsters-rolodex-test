fdb27752cdfe9179cafd0ebc3d91cbee2e5e0f05
shea-paris <shea.ssmp@gmail.com>
2019-09-23 12:17:08 +1200

    Updates

---
+++ precache-manifest.d444dce65a8a587693aad20a142161fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00858810c0da860737d246ff5e469991",
+    "url": "/monsters-rolodex-test/index.html"
+  },
+  {
+    "revision": "595a1d5f115ec6d11b2b",
+    "url": "/monsters-rolodex-test/static/css/main.b4bd226d.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex-test/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "595a1d5f115ec6d11b2b",
+    "url": "/monsters-rolodex-test/static/js/main.7e18d595.chunk.js"
+  },
+  {
+    "revision": "3b11ec029ffebcabc5fe",
+    "url": "/monsters-rolodex-test/static/js/runtime~main.05510755.js"
+  }
+]);
